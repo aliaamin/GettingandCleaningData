a6af0c157612985d492029119ed7863db1383993
aliaamin <cakhairia@gmail.com>
2015-03-15 22:03:38 +0100

    update

--- run_analysis.R
+++ run_analysis.R
@@ -12,28 +12,28 @@ runA <- function() {
         # step5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
         # step 0.1 load data
-                f1 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/features.txt"
+                f1 <- paste(getwd(),"UCI HAR Dataset/features.txt", sep ="/")        
                 d_feature = read.table(f1, sep=" ")
-                
-                f2 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/train/y_train.txt"
+
+                f2 <- paste(getwd(),"UCI HAR Dataset/train/y_train.txt", sep ="/")        
                 d_ytrain = read.table(f2, sep=" ")
 
-                f3 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/train/subject_train.txt"
+                f3 <- paste(getwd(),"UCI HAR Dataset/train/subject_train.txt", sep ="/")        
                 d_subjecttrain = read.table(f3, sep=" ")
-                
-                f4 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/train/X_train.txt"
+        
+                f4 <- paste(getwd(),"UCI HAR Dataset/train/X_train.txt", sep ="/")        
                 d_xtrain = read.table(f4, as.is= TRUE)
 
-                f5 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/activity_labels.txt"
+                f5 <- paste(getwd(),"UCI HAR Dataset/activity_labels.txt", sep ="/")        
                 d_activitylabel = read.table(f5, sep=" ", na.strings = "NA", fill = TRUE)
 
-                f6 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/test/X_test.txt"
+                f6 <- paste(getwd(),"UCI HAR Dataset/test/X_test.txt", sep ="/")        
                 d_xtest = read.table(f6, as.is= TRUE)
 
-                f7 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/test/y_test.txt"
+                f7 <- paste(getwd(),"UCI HAR Dataset/test/y_test.txt", sep ="/")                
                 d_ytest = read.table(f7, sep=" ", na.strings = "NA", fill = TRUE)
-                
-                f8 <- "E:/mooc/excercise/3_cleaningdata/project/UCI HAR Dataset/test/subject_test.txt"
+
+                f8 <- paste(getwd(),"UCI HAR Dataset/test/subject_test.txt", sep ="/")        
                 d_subjecttest = read.table(f8, sep=" ", na.strings = "NA", fill = TRUE)
                 
                 
@@ -94,14 +94,15 @@ runA <- function() {
         combidata <- rbind(combidata,combidata2)
         combidata <- arrange(combidata, subject) 
 
+        # step2: Extracts only the measurements on the mean and standard deviation for each measurement. 
+        # step3: Uses descriptive activity names to name the activities in the data set
+        # step4: Appropriately labels the data set with descriptive variable names. 
+
+        # step5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
         secondcombidata <- group_by(combidata, subject, activity)
         x <- summarize(secondcombidata)
-        print(x)
+#        print(x)
 
-#        requiredColumnsList <- grep("mean",d_feature[[2]], ignore.case = TRUE)
-#        print(length(requiredColumnsList))
-#        requiredColumnsList <- grep("std",d_feature[[2]], ignore.case = TRUE)
-#        print(length(requiredColumnsList))
 
 
         return(x)       
